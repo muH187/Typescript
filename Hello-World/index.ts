7b18feae5d5326e94753b682009145fc9e10894d
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-05 00:13:07 +0500

    learning typscript

---
+++ Hello-World/index.ts
@@ -0,0 +1,8 @@
+console.log("Hello World")
+console.log("My name is Muhammad Ali");
+console.log("I'm from Karachi Pakistan")
+console.log("I love socializing");
+console.log(10 + 30);
+console.log("Thank you Kamran Khan Tessori for giving us this opportunity");
+
+
