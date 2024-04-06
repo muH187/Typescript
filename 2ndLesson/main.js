13a5b224148686c61370f58412620085d6c1203b
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-06 12:31:57 +0500

    learning typscript and Git

---
+++ 2ndLesson/main.js
@@ -0,0 +1,5 @@
+var first_name = "Muhammad Ali";
+var detail = "He is a Student";
+console.log(first_name, detail);
+var age = 19;
+console.log(age);
---
+++ 2ndLesson/main.ts
@@ -0,0 +1,7 @@
+let first_name = "Muhammad Ali"
+let detail = "He is a Student"
+
+console.log(first_name, detail);
+
+let age = 19
+console.log(age);
