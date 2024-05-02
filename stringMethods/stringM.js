88b9f12e322047a5c9bcb8fd58305ecc76cb55ac
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-02 15:16:12 +0500

    string methods

---
+++ stringMethods/stringM.js
@@ -0,0 +1,46 @@
+// 1. charAt:
+// let string = "Hello World"
+
+// let newString = string.charAt(0) + string.charAt(6) 
+
+// console.log(newString)
+
+// 2. concat:
+// let string1 = "Hello! My name is Muhammad"
+// let string2 = "Ali"
+// let concatenatedString = string1.concat(" ", string2)
+// console.log(concatenatedString)
+
+// 3. indexOf:
+// let string = "I'm Muhammad Ali student of Chinese language at Karachi University."
+// let string2 = `Index of Karachi is at ${string.indexOf("Karachi")}`
+// console.log(string2)
+
+// 4. lastIndexOf:
+// let string = "I'm Muhammad Ali student of Economics at Allama Iqbal Open University"
+// console.log("The last occurrence  of y is at", string.lastIndexOf("y"))
+
+// 5. slice:
+// let string = "I'm Muhammad Ali enthusiast of programming and digital marketing"
+// let string2 = string.slice(4, 16) + " " + string.slice(31, 42)
+// console.log(string2)
+
+// 6. split:
+// let string = "This is Muhammad Ali learning typescript."
+// let string2 = string.split(" ")
+// console.log(string2)
+
+// 7. substring:
+// let string = "He is learning programming language called TypeScript"
+// console.log(string.substring(15, 26))
+
+// 8. substr:
+// let string = "TypeScript, JavaScript, Python"
+// let string2 = string.substr(0, 10) + " " + string.substr(24, 6) 
+// console.log(string2)
+
+// 9. toLowerCase 10. toUpperCase
+// let string = "pakistan"
+// let string2 = "KARACHI"
+// console.log(string.toUpperCase())
+// console.log(string2.toLowerCase())
